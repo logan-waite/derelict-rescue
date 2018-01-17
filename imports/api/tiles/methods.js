3b440a6dc66460c9863f787033fcf65629f0b3b6
Logan Waite <logan@itovi.com>
2018-01-17 15:06:18 -0700

    doors
    Discovering a tile automatically renders a door in the direction you came from and sets it to discovered, then fills in the rest of the doors.

--- imports/api/tiles/methods.js
+++ imports/api/tiles/methods.js
@@ -3,24 +3,16 @@ import { check, Match } from 'meteor/check';
 import { Tiles, DiscoveredTiles } from './tiles.js';
 
 Meteor.methods({
-  'discoveredTiles.insert' (position) {
+  'discoveredTiles.insert' (position, prev_tile_id) {
 
     check(position, Match.ObjectIncluding({x:Number, y:Number}));
+    check(prev_tile_id, String);
 
-    var tile = discoverTile();
+    var prev_tile_pos = getTilePosition(prev_tile_id);
 
+    var tile = discoverTile(prev_tile_pos, position);
 
-
-    // var already_discovered = DiscoveredTiles.find().fetch();
-    //
-    // already_discovered.forEach(function(dtile) {
-    //   // console.log(dtile);
-    // })
-
-    return DiscoveredTiles.insert({
-      position : position,
-      tile : tile
-    })
+    return DiscoveredTiles.insert(tile);
   },
 
   'discoveredTiles.one' ({ _id }) {
@@ -28,10 +20,19 @@ Meteor.methods({
   }
 });
 
-function discoverTile() {
-  var tile = getRandomTile()
+function getTilePosition(tile_id) {
+  var tile = DiscoveredTiles.findOne({_id:tile_id});
+  return tile.position;
+}
 
-  return tile;
+function discoverTile(prev_tile_pos, position) {
+  // var tile = getRandomTile()
+  var tile = Tiles.findOne({"tile_id" : 4});
+
+  // Set doors for this tile
+  var doors = generateDoors(tile, prev_tile_pos, position);
+
+  return {tile : tile, doors : doors, position : position};
 }
 
 function getRandomTile() {
@@ -39,5 +40,84 @@ function getRandomTile() {
 
   return Tiles.findOne({"tile_id" : random_tile_id});
 
-  // return tile;
+  return tile;
+}
+
+function setDoorToDiscovered() {
+
+}
+
+function generateDoors(tile, prev_tile_pos, new_pos) {
+  var door_number = {u:0, d:0, l:0, r:0};
+  var doors = [];
+  // Determine where the door we passed through is.
+  var door_side;
+  if(new_pos.x != prev_tile_pos.x) {
+    if (new_pos.x < prev_tile_pos.x) {
+      door_number.r++
+      doors.push({x : 1, d : true})
+    } else {
+      door_number.l++
+      doors.push({x : -1, d : true})
+    }
+  } else if (new_pos.y != prev_tile_pos.y) {
+    if (new_pos.y < prev_tile_pos.y) {
+      door_number.u++;
+      doors.push({y : 1, d : true})
+    } else {
+      door_number.d++;
+      doors.push({y : -1, d : true})
+    }
+  }
+
+  for (var i = 0; i < (tile.doors - 1); i++) {
+    var num = randomNumber(50, -50);
+    // Odd numbers are x, even are y
+    if (num % 2 == 0) {
+      var side = Math.sign(num);
+      if (side < 0) {
+        if (door_number.d < tile.size.y) {
+          door_number.d++
+          doors.push({x : side, d:false});
+        } else {
+          i--;
+        }
+      } else {
+        if (door_number.u < tile.size.y) {
+          door_number.u++
+          doors.push({x : side, d:false});
+        } else {
+          i--;
+        }
+      }
+    } else {
+      var side = Math.sign(num);
+      if (side < 0) {
+        if (door_number.l < tile.size.x) {
+          door_number.l++
+          doors.push({y : side, d:false});
+        } else {
+          i--;
+        }
+      } else {
+        if (door_number.r < tile.size.x) {
+          door_number.r++
+          doors.push({y : side, d:false});
+        } else {
+          i--;
+        }
+      }
+    }
+  }
+
+  return doors;
+
+}
+
+function randomNumber(max, min) {
+  var num = Math.floor(Math.random() * (max - min) + min);
+  if (num == 0) {
+    num = randomNumber(max, min)
+  }
+  return num;
 }
--- imports/ui/components/game-board/game-board.js
+++ imports/ui/components/game-board/game-board.js
@@ -29,9 +29,7 @@ Template.gameBoard.helpers({
   currentTiles() {
     $('.board-tile').remove();
     var tiles = DiscoveredTiles.find().fetch();
-    console.log(tiles);
     tiles.forEach(function(tile) {
-      console.log(tile);
       var btile = tile.tile;
       // create board-tile template
       var bt_object = $("<div class='board-tile' id="+tile._id+"></div>");
@@ -48,57 +46,54 @@ Template.gameBoard.helpers({
         }
       });
       // Add the doors and put them where they belong.
-      // tile.doors.forEach(function(position) {
-      //   // Figure out the side the door should be on.
-      //   var door = $("<div class='door'></div>");
-      //   // Start with which side the door will be on (rooms will be squre for now.)
-      //   if (position.x !== undefined) // horizontal
-      //   {
-      //     door.addClass("horizontal")
-      //     if (position.x < 0) {
-      //       door.css("left", "-2px");
-      //     } else {
-      //       door.css("right", "-2px")
-      //     }
-      //
-      //     var block = 0;
-      //     // check if we've been told where to put the door
-      //     if (position.b !== undefined) { // basically, this means the room is bigger than 1 block.
-      //       block = position.b;
-      //     }
-      //     // Doors should be in the middle of the tile, which means a space of 25px for a single tile.
-      //     // Bigger rooms are squares of bigger tiles, so we add 100 for each block we need to move.
-      //     door.css("top", ((block * 100) + 25) + "px")
-      //   }
-      //   else if (position.y !== undefined) // vertical
-      //   {
-      //     door.addClass("vertical")
-      //     if (position.y > 0) {
-      //       door.css("top", "-2px")
-      //     } else {
-      //       door.css("bottom", "-2px")
-      //     }
-      //
-      //     var block = 0;
-      //     // check if we've been told where to put the door
-      //     if (position.b !== undefined) { // basically, this means the room is bigger than 1 block.
-      //       block = position.b;
-      //     }
-      //     // Doors should be in the middle of the tile, which means a space of 25px for a single tile.
-      //     // Bigger rooms are squares of bigger tiles, so we add 100 for each block we need to move.
-      //     door.css("left", ((block * 100) + 25) + "px")
-      //   }
-      //   bt_object.append(door);
-      // })
+      tile.doors.forEach(function(position) {
+        // Figure out the side the door should be on.
+        var door = $("<div class='door'></div>");
+        // Start with which side the door will be on (rooms will be squre for now.)
+        if (position.x !== undefined) // horizontal
+        {
+          door.addClass("horizontal")
+          if (position.x < 0) {
+            door.css("left", "-2px");
+          } else {
+            door.css("right", "-2px")
+          }
+
+          var block = 0;
+          // check if we've been told where to put the door
+          if (position.b !== undefined) { // basically, this means the room is bigger than 1 block.
+            block = position.b;
+          }
+          // Doors should be in the middle of the tile, which means a space of 25px for a single tile.
+          // Bigger rooms are squares of bigger tiles, so we add 100 for each block we need to move.
+          door.css("top", ((block * 100) + 25) + "px")
+        }
+        else if (position.y !== undefined) // vertical
+        {
+          door.addClass("vertical")
+          if (position.y > 0) {
+            door.css("top", "-2px")
+          } else {
+            door.css("bottom", "-2px")
+          }
+
+          var block = 0;
+          // check if we've been told where to put the door
+          if (position.b !== undefined) { // basically, this means the room is bigger than 1 block.
+            block = position.b;
+          }
+          // Doors should be in the middle of the tile, which means a space of 25px for a single tile.
+          // Bigger rooms are squares of bigger tiles, so we add 100 for each block we need to move.
+          door.css("left", ((block * 100) + 25) + "px")
+        }
+        bt_object.append(door);
+      })
       // Then move it to where it needs to go using the position
       // We set the positions multiplied by 99 so the borders match
       // To move it up, set the bottom position to (y * 99).
       // To move it right or left, add (x * 99) to -50 (the default margin-left) (x could be a negative number)
-      console.log(tile.position);
-      var up = (tile.position.y * 99);
-      var sideways = (tile.position.x * 99) + -50;
-      console.log("up after: " + up);
-      console.log("sideways after: " + sideways);
+      var up = (tile.position.y * 100);
+      var sideways = (tile.position.x * 100) + -50;
       bt_object.css({"bottom": up, "margin-left": sideways});
     })
   }
@@ -118,57 +113,76 @@ Template.gameBoard.events({
     // console.log(event.target)
   },
   // Hovering events (mostly for showing potential routes from this tile)
-  'mouseenter .board-tile'(event) {
+  'dropover .board-tile'(event, ui) {
     event.preventDefault();
+    hidePossibleTiles($(event.target).attr("id"))
+
+      // console.log("dropover!");
+      // console.log(ui);
+      // console.log(event.target);
+      targetTile = $(event.target).attr("id");
     // Start off by getting the position of the tile we're hovering over
     var htile = $(event.target);
     var tile_id = htile.attr("id");
     var tile = DiscoveredTiles.findOne({_id:tile_id})
     // var position = tile.position;
     // We're going to want to create a possible tile for each door
-    tile.doors.forEach(function(door_position) {
-      // first we need to check if the door has already been "discovered"
-      if (!door_position.d) {
-        pt_object = $("<div class='possible-tile t_" + tile._id + "'></div>");
-        var bottom, left;
-
-        if (door_position.x !== undefined) { // On one of the sides
-          bottom = tile.position.y // This will be affected by the "b" parameter
-          left = ((tile.position.x + door_position.x) * 100) + -50;
-        }
-        else if (door_position.y !== undefined) { // On top or bottom
-          bottom = ((tile.position.y + door_position.y) * 100);
-          left = (tile.position.x * 100) + -50; // This will be affected by the "b" parameter
-        }
-        $('#board').append(pt_object);
-        pt_object.droppable({
-          accept: ".player",
-          drop : function(event, ui) {
-            // When the player drops on this, we need to find its position and save a new discovered tile.
-            var target = $(event.target);
-            var y_pos = parseInt(target.css("bottom")) / 100;
-            var x_pos = (parseInt(target.css("margin-left")) + 50) / 100;
-            newDiscoveredTile({x:x_pos, y:y_pos});
-          }
-        })
-        // Then we do the same thing as if adding a tile, just add 1 to the positions before multiplying them.
-        pt_object.css({"bottom": bottom, "margin-left": left}).show();
-      }
-    });
-  },
-  'mouseleave .board-tile'(event) {
-    event.preventDefault();
-    var htile = $(event.target);
-    // console.log("leaving!")
-    var tile_id = htile.attr("id");
-    // $(".t_"+tile_id).hide().remove();
+    showPossibleTiles(tile);
   },
   'dragstart .player' (event, ui) {
     $(event.target).css({"position": "relative"});
+  },
+  'dragstop .player' (event, ui) {
+    hidePossibleTiles()
   }
 })
 
-function newDiscoveredTile(position) {
-  Meteor.call("discoveredTiles.insert", position);
+function showPossibleTiles(tile) {
+  tile.doors.forEach(function(door_position) {
+    // first we need to check if the door has already been "discovered"
+    if (!door_position.d) {
+      pt_object = $("<div class='possible-tile t_" + tile._id + "'></div>");
+      var bottom, left;
+
+      if (door_position.x !== undefined) { // On one of the sides
+        bottom = tile.position.y * 100 // This will be affected by the "b" parameter
+        left = ((tile.position.x + door_position.x) * 100) + -50;
+      }
+      else if (door_position.y !== undefined) { // On top or bottom
+        bottom = ((tile.position.y + door_position.y) * 100);
+        left = (tile.position.x * 100) + -50; // This will be affected by the "b" parameter
+      }
+      $('#board').append(pt_object);
+
+      // bind all the possible-tile events now because they weren't around earlier to bind.
+      pt_object.droppable({
+        accept: ".player",
+        drop : function(event, ui) {
+          // When the player drops on this, we need to find its position and save a new discovered tile.
+          var target = $(event.target);
+          var tile_classes = target.attr("class");
+          var id_pattern = /t_([A-Za-z0-9]+)/g;
+          var dtile_id = id_pattern.exec(tile_classes)[1]
+          var y_pos = parseInt(target.css("bottom")) / 100;
+          var x_pos = (parseInt(target.css("margin-left")) + 50) / 100;
+          newDiscoveredTile({x:x_pos, y:y_pos}, dtile_id);
+        }
+      })
+      // Then we do the same thing as if adding a tile, just add 1 to the positions before multiplying them.
+      pt_object.css({"bottom": bottom, "margin-left": left}).show();
+    }
+  });
+}
+
+function hidePossibleTiles(tile_id) {
+  if(tile_id !== undefined) {
+    $(".possible-tile:not(.t_"+tile_id+")").hide().remove();
+  } else {
+    // $('.possible-tile').hide().remove();
+  }
+}
+
+function newDiscoveredTile(position, tile_id) {
+  Meteor.call("discoveredTiles.insert", position, tile_id);
 }
 
